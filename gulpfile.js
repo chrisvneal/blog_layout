de906b47a1ca8d07107c09b3c31cc7698a05bf1b
Chris Neal <chrisvneal@gmail.com>
2019-02-10 21:26:41 -1000

    Add copy html task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let sass_folder = 'src/scss/**/*.scss';
+let css_folder = ('dist/css');
 
+// convert sass to css
 gulp.task('sass', function() {
-  return gulp.src(('src/scss/**/*.scss'))
+  return gulp.src(sass_folder)
     .pipe(sass())
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest(css_folder))
 });
 
+gulp.task('html', function() {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+
+// watch functions
 gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch(sass_folder, gulp.series('sass'));
+  gulp.watch('src/**/*.html', gulp.series('html'))
 });
