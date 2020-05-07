9eedb01e5fb02295ae87ea685a2912d6ce025223
Chris Neal <chrisvneal@gmail.com>
2020-05-06 19:51:04 -1000

    Installed BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -5,23 +5,35 @@ const css_folder = 'dist/css';
 const html_folder = 'src/**/*.html';
 const browserSync = require('browser-sync').create();
 
+// browserSync function
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+});
+
 // convert sass to css
 gulp.task('sass', function() {
   return gulp.src(sass_folder)
     .pipe(sass())
     .pipe(gulp.dest(css_folder))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('html', function() {
   return gulp.src(html_folder)
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
 
+
 // watch functions
-gulp.task('watch', function() {
+gulp.task('watch', gulp.parallel('html', 'sass', 'browserSync'), function() {
   gulp.watch(sass_folder, gulp.series('sass'));
-  gulp.watch(html_folder, gulp.series('html'))
+  gulp.watch(html_folder, gulp.series('html'));
 });
 
 gulp.task('default', gulp.series('watch'));
