1790907a8f4ba98fc2ff3fe72be6f65136a3a092
Chris Neal <chrisvneal@gmail.com>
2020-05-06 20:16:32 -1000

    Add new gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,103 @@
+'use strict';
+
+// plugins
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const sass_folder = 'src/scss/**/*.scss';
-const css_folder = 'dist/css';
-const html_folder = 'src/**/*.html';
+sass.compiler = require('node-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
+const changed = require('gulp-changed');
+const imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
+const cleanCSS = require('gulp-clean-css');
+const autoprefix = require('gulp-autoprefixer');
 
-// browserSync function
-gulp.task('browserSync', function() {
+// directories
+const dir = {
+  // sources
+  scss_src: 'src/scss/**/*.scss',
+  html_src: 'src/*.html',
+  js_src: 'src/js/**/*.js',
+  images_src: 'src/img/**/*.+(png|jpg|gif)',
+
+  // destinations
+  dist: 'dist',
+  css_dist: 'dist/css',
+  js_dist: 'dist/js',
+  images_dist: 'dist/img'
+}
+
+/*********** Tasks ***********/
+/****************************/
+
+// spin up a web server via browser-sync
+function browser_sync() {
   browserSync.init({
     server: {
-      baseDir: 'dist'
+      baseDir: dir.dist
     }
-  });
-});
+  })
+}
 
-// convert sass to css
-gulp.task('sass', function() {
-  return gulp.src(sass_folder)
-    .pipe(sass())
-    .pipe(gulp.dest(css_folder))
-    .pipe(browserSync.reload({stream: true}));
-});
+// copy html files to dist folder
+function html() {
+  return gulp.src(dir.html_src)
+    .pipe(changed(dir.dist))
+    .pipe(gulp.dest(dir.dist))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+}
 
-gulp.task('html', function() {
-  return gulp.src(html_folder)
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.reload({stream: true}));
-});
+// convert scss to css (dist)
+function scss() {
+  return gulp.src(dir.scss_src)
+    
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefix({
+      cascade: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(dir.css_dist))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+}
 
+// copy js files
+function js() {
+  return gulp.src(dir.js_src)
+    .pipe(changed(dir.js_dist))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest(dir.js_dist))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+}
 
+// convert images and copy to img folder 
+function images() {
+  return gulp.src(dir.images_src)
+    .pipe(changed(dir.images_dist))
+    .pipe(imagemin())
+    .pipe(gulp.dest(dir.images_dist));
+}
 
-// watch functions
-gulp.task('watch', gulp.parallel('html', 'sass', 'browserSync'), function() {
-  gulp.watch(sass_folder, gulp.series('sass'));
-  gulp.watch(html_folder, gulp.series('html'));
-});
+// watch sass, javascript, and html changes
+gulp.task('watch', gulp.parallel(html, scss, js, images, browser_sync, function() {
+
+  // watch scss files for changes
+  gulp.watch(dir.scss_src, scss);
+
+  // watch html files for changes
+  gulp.watch(dir.html_src, html);
+
+  // watch javascript files for changes
+  gulp.watch(dir.js_src, js);
+}));
 
 gulp.task('default', gulp.series('watch'));
